5b8187bf876d4c63412f8da0a58d252cae48f00a
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2025-04-06 21:42:22 -0300

    Update README

---
+++ src/main/java/br/com/dio/exception/CardFinishedException.java
@@ -0,0 +1,8 @@
+package br.com.dio.exception;
+
+public class CardFinishedException extends RuntimeException{
+
+    public CardFinishedEEException(final String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/br/com/dio/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.dio.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundEEException(String message) {
+        super(message);
+    }
+
+}
